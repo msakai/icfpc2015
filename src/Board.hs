5c255ce37ee96c5163e1499d2dc773a5c0821142
Nobuo Yamashita <nobsun@sampou.org>
2015-08-08 07:14:25 +0900

    lockUnit :: Board -> Unit -> Board

--- src/Board.hs
+++ src/Board.hs
@@ -1,7 +1,8 @@
 module Board where
 
 import Data.Ix (inRange)
-import Data.Set (Set(..),notMember)
+import Data.Set (Set(..))
+import qualified Data.Set as Set (notMember,insert)
 
 import Cell
 import Unit
@@ -12,8 +13,11 @@ data Board = Board { cols :: Number, rows :: Number, fulls :: Set Cell }
 
 valid :: Board -> Unit -> Maybe Unit
 valid b u 
- = if and (map (flip notMember fs) cs) && all (inRange (cell (0,0), cell (cols b -1, rows b -1))) cs
+ = if and (map (flip Set.notMember fs) cs) && all (inRange (cell (0,0), cell (cols b -1, rows b -1))) cs
    then Just u else Nothing
   where
     fs = fulls b
     cs = members u
+
+lockUnit :: Board -> Unit -> Board
+lockUnit b u = b { fulls = foldr Set.insert (fulls b) (members u) }
