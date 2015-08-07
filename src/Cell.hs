c445b3ee256d86d03c5e462a90d588785289a0db
Masahiro Sakai <masahiro.sakai@gmail.com>
2015-08-08 00:18:13 +0900

    add cell position calculation functions

--- src/Cell.hs
+++ src/Cell.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE
     OverloadedStrings
   , DeriveGeneric
+  , NamedFieldPuns
  #-}
 module Cell where
 
@@ -13,3 +14,33 @@ data Cell = Cell { x :: Number , y :: Number } deriving (Show, Eq, Generic)
 
 instance FromJSON Cell
 instance ToJSON Cell
+
+eastCell :: Cell -> Cell
+eastCell c@Cell{ x } = c{ x = x + 1 }
+
+westCell :: Cell -> Cell
+westCell c@Cell{ x } = c{ x = x - 1 }
+
+southEastCell :: Cell -> Cell
+southEastCell c@Cell{ x, y } = Cell{ x = if even y then x else x + 1, y = y + 1 }
+
+southWestCell :: Cell -> Cell
+southWestCell c@Cell{ x, y } = Cell{ x = if even y then x - 1 else x, y = y + 1 }
+
+northEastCell :: Cell -> Cell
+northEastCell c@Cell{ x, y } = Cell{ x = if even y then x else x + 1, y = y - 1 }
+
+northWestCell :: Cell -> Cell
+northWestCell c@Cell{ x, y } = Cell{ x = if even y then x - 1 else x, y = y - 1 }
+
+testCellDir = and
+  [ southEastCell (Cell 1 1) == Cell 2 2
+  , southWestCell (Cell 1 1) == Cell 1 2
+  , northEastCell (Cell 1 1) == Cell 2 0
+  , northWestCell (Cell 1 1) == Cell 1 0
+
+  , southEastCell (Cell 1 2) == Cell 1 3
+  , southWestCell (Cell 1 2) == Cell 0 3
+  , northEastCell (Cell 1 2) == Cell 1 1
+  , northWestCell (Cell 1 2) == Cell 0 1
+  ]
