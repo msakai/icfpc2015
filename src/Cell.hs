9cafed819b90521b6c73f6d8fcc326cfabd0b97f
Nobuo Yamashita <nobsun@sampou.org>
2015-08-08 01:20:21 +0900

    deriving Ord

--- src/Cell.hs
+++ src/Cell.hs
@@ -10,7 +10,7 @@ import GHC.Generics
 
 import Types
 
-data Cell = Cell { x :: Number , y :: Number } deriving (Show, Eq, Generic)
+data Cell = Cell { x :: Number , y :: Number } deriving (Show, Eq, Ord, Generic)
 
 instance FromJSON Cell
 instance ToJSON Cell
