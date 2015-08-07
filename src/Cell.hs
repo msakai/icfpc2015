8475cf9d0773ed862e363adec95f6049e3e634ad
cutsea110 <cutsea110@gmail.com>
2015-08-07 23:17:17 +0900

    make Cell FromJSON instance

--- src/Cell.hs
+++ src/Cell.hs
@@ -1,5 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Cell where
 
+import Control.Applicative ((<$>), (<*>))
+import Data.Aeson
 import Types
 
 data Cell = Cell { x :: Number , y :: Number } deriving (Show, Eq)
+
+instance FromJSON Cell where
+  parseJSON (Object v) = Cell <$> v .: "x" <*> v .: "y"
