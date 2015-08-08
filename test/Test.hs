ed5221e7e10fff1fb9f851f25563dcceb5bec5f1
Masahiro Sakai <masahiro.sakai@gmail.com>
2015-08-08 09:15:47 +0900

    add a test case for readProblem function

--- test/Test.hs
+++ test/Test.hs
@@ -3,6 +3,8 @@
  #-}
 module Main where
 
+import Data.Maybe
+
 import Test.Tasty
 import Test.Tasty.QuickCheck
 import Test.Tasty.HUnit
@@ -13,6 +15,7 @@ import Types
 import qualified Command
 import qualified Cell
 import qualified PRNG
+import qualified Util
 
 case_moveCell_1 = Cell.moveCell FaceSE (Cell.Cell 1 1) @?= Cell.Cell 2 2
 case_moveCell_2 = Cell.moveCell FaceSW (Cell.Cell 1 1) @?= Cell.Cell 1 2
@@ -38,6 +41,10 @@ case_turnCell_12 = Cell.turnCell Command.CCW (Cell.Cell 2 3) (Cell.Cell 2 5) @?=
 
 case_PRNG_generate =
   take 10 (PRNG.generate 17) @?= [0,24107,16552,12125,9427,13152,21440,3383,6873,16117]
+
+case_readProblem = do
+  r <- Util.readProblem "problems/problem_18.json"
+  isJust r @? "failed to parse problem_18.json"
 
 main :: IO ()
 main = $(defaultMainGenerator)
