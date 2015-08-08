e7f657518969bdff87d93824997e9a4b8f36092e
cutsea110 <cutsea110@gmail.com>
2015-08-08 14:51:15 +0900

    dummy implement power_score

--- src/Score.hs
+++ src/Score.hs
@@ -9,3 +9,9 @@ move_score size ls ls_old = points + line_bonus
       line_bonus = if ls_old > 1
                    then ((ls_old - 1) * points `div` 10)
                    else 0
+
+power_score :: String -> String -> Pt
+power_score pw cmds = 2 * length pw * reps + power_bonus
+    where
+      reps = 0 -- TODO: match pw for cmds
+      power_bonus = if reps > 0 then 300 else 0
