e9115f1fb9199019b0825fb7bf9c121e18a65e9d
cutsea110 <cutsea110@gmail.com>
2015-08-09 19:01:40 +0900

    define high level utility burst function

--- src/Play.hs
+++ src/Play.hs
@@ -35,7 +35,10 @@ genTag n sd = do
   now <- getCurrentTime
   zone <- getCurrentTimeZone
   let (TimeOfDay h m s) = localTimeOfDay $ utcToLocalTime zone now
-  return $ "problem" ++ show n ++ "-" ++ "-seed[" ++ show sd ++ "]-"++ show h ++ "-" ++ show m ++ "-" ++ show s
+  return $ "problem" ++ show n ++ "-seed[" ++ show sd ++ "]-"++ show h ++ "-" ++ show m ++ "-" ++ show s
+
+burst :: ProblemId -> Int -> Int -> Player -> IO ()
+burst pid sdN cyc ani = mapM_ (\(n,sd) -> autoPlay pid sd ani) [(n, sd) | n <- [1..cyc], sd <- [0..sdN-1]]
 
 autoPlay :: ProblemId -> SeedNo -> Player -> IO ()
 autoPlay n sd ani = testPlay n sd (play' 0 ani)
