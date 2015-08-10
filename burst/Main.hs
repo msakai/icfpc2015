2bcb994b041028ab7cf0e0fb01c236b22e39b328
Masahiro Sakai <masahiro.sakai@gmail.com>
2015-08-10 15:00:03 +0900

    add Reynolds support to burst command

--- burst/Main.hs
+++ burst/Main.hs
@@ -1,18 +1,21 @@
+import Data.Char
 import System.Environment
 import Play
 import qualified Tactics.RandomWalk as RandomWalk
 import qualified Tactics.RandomWalk2 as RandomWalk2
-import qualified Tactics.RandomZigZag as RandomZigZag    
+import qualified Tactics.RandomZigZag as RandomZigZag
+import qualified Tactics.Reynolds as Reynolds
 
 main = do
   args <- getArgs
   case args of
     [problemid, cycle, playerName] -> do
-      let player =
-            case playerName of
-              "RandomWalk" -> RandomWalk.newPlayer
-              "RandomWalk2" -> RandomWalk2.newPlayer
-              "RandomZigZag" -> RandomZigZag.newPlayer
+      let newPlayer =
+            case map toLower playerName of
+              "randomwalk" -> RandomWalk.newPlayer
+              "randomwalk2" -> RandomWalk2.newPlayer
+              "randomzigzag" -> RandomZigZag.newPlayer
+              "reynolds" -> Reynolds.newPlayer 300
               _ -> error "unknown player name"
-      burst (read problemid) (read cycle) player
+      burst (read problemid) (read cycle) newPlayer
     _ -> error "USAGE: burst problemId cycle player"
