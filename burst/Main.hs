b18d5096758b337a88b0226e7e779695b6ff8fd9
Masahiro Sakai <masahiro.sakai@gmail.com>
2015-08-10 19:28:01 +0900

    improve burst command

--- burst/Main.hs
+++ burst/Main.hs
@@ -1,5 +1,9 @@
+import Control.Monad
 import Data.Char
+import System.Console.GetOpt
 import System.Environment
+import System.Exit
+    
 import Play
 import qualified Tactics.RandomWalk as RandomWalk
 import qualified Tactics.RandomWalk2 as RandomWalk2
@@ -7,17 +11,59 @@ import qualified Tactics.RandomZigZag as RandomZigZag
 import qualified Tactics.Reynolds as Reynolds
 import qualified Tactics.SearchAllLocking as SearchAllLocking
 
+data Options
+  = Options
+  { optDisplayMode :: DisplayMode
+  , optPlayer :: String
+  , optRepeatNum :: Int
+  , optReynoldsN :: Int                   
+  }
+
+defaultOptions :: Options
+defaultOptions
+  = Options
+  { optDisplayMode = DisplayAll
+  , optPlayer      = "reynolds"
+  , optRepeatNum   = 1
+  , optReynoldsN   = 300
+  }
+
+options :: [OptDescr (Options -> Options)]
+options =
+  [ Option [] ["display-mode"] (ReqArg (\val opt -> opt{ optDisplayMode = parseDisplayMode val }) "str") "display mode: all, locked, none"
+  , Option [] ["player"] (ReqArg (\val opt -> opt{ optPlayer = val }) "str") "player name"
+  , Option [] ["repeat"] (ReqArg (\val opt -> opt{ optRepeatNum = read val }) "int") "number of repetition (default: 1)"
+  , Option [] ["reynolds-n"] (ReqArg (\val opt -> opt{ optReynoldsN = read val }) "int") ("(default: " ++  show (optReynoldsN defaultOptions) ++ ")")
+  ]
+  where
+    parseDisplayMode "all" = DisplayAll
+    parseDisplayMode "locked" = DisplayLocked
+    parseDisplayMode "none" = DisplayNone
+    parseDisplayMode s = error $ "unknown display mode: " ++ s
+    
 main = do
   args <- getArgs
-  case args of
-    (problemid:cycle:playerName:xs) -> do
+  case getOpt Permute options args of
+    (_,_,errs@(_:_)) -> do
+      mapM_ putStrLn errs
+      exitFailure
+
+    (o,args2,[]) -> do
+      let opt = foldl (flip id) defaultOptions o
       let newPlayer =
-            case map toLower playerName of
+            case map toLower (optPlayer opt) of
               "randomwalk" -> RandomWalk.newPlayer
               "randomwalk2" -> RandomWalk2.newPlayer
               "randomzigzag" -> RandomZigZag.newPlayer
-              "reynolds" -> Reynolds.newPlayer (read (head xs))
+              "reynolds" -> Reynolds.newPlayer (optReynoldsN opt)
               "searchalllocking" -> return SearchAllLocking.player
               _ -> error "unknown player name"
-      burst (read problemid) (read cycle) newPlayer
-    _ -> error "USAGE: burst problemId cycle player"
+      if null args2 then
+        help
+      else do
+        forM_ args2 $ \problemid -> do
+          burst (read problemid) (optRepeatNum opt) (optDisplayMode opt) newPlayer
+
+    _ -> help
+
+help = putStrLn $ usageInfo "USAGE: burst [OPTIONS] problemId1 problemId2" options
